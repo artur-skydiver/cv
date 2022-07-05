75b385c35d46770ce567d4fa7b696b743ac3d54d
Artur Blieshcheiev <bleshcheev90@gmail.com>
2022-07-05 04:33:22 +0300

    Github link

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Extra from 'containers/Extra';
 
 import s from './App.module.scss';
 
+const github = 'https://github.com/artur-skydiver/cv';
+
 function App() {
   useEffect(() => {
     document.documentElement.removeAttribute('data-theme-initing');
@@ -14,6 +16,15 @@ function App() {
 
   return (
     <div className={s.root}>
+      <a
+        href={github}
+        target="_blank"
+        rel="noreferrer"
+        title="This CV on GitHub"
+        className={s.github}
+      >
+        {github}
+      </a>
       <Main />
       <Extra />
     </div>
